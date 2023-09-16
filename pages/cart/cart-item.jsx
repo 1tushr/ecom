cbb99aec8ba23534e23d8a4f698a0966ece77e20
Tushar Verma <110741618+1tushr@users.noreply.github.com>
2023-09-17 04:32:25 +0530

    Add files via upload

---
+++ pages/cart/cart-item.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+
+export const CartItem = ({data}) => {
+  const { id, productName, price, productImage } = data;
+  const { cartItems, addToCart, removeFromCart, updateCartItemCount } =
+    useContext(ShopContext);
+
+  return (
+    <div className="cartItem">
+      <img src={productImage} />
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p> Price: ${price}</p>
+        <div className="countHandler">
+          <button style={{border:"none",background:"white"}} onClick={() => removeFromCart(id)}> - </button>
+          <input
+            value={cartItems[id]}
+            onChange={(e) => updateCartItemCount(Number(e.target.value), id)}
+          />
+          <button style={{border:"none",background:"white"}} onClick={() => addToCart(id)}> + </button>
+        </div>
+      </div>
+    </div>
+  );
+};
